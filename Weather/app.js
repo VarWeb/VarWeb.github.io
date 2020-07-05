4d230125744c75c511065f7edb8ebc7dc67f47dd
VarWeb <63775121+VarWeb@users.noreply.github.com>
2020-07-05 23:37:08 +0530

    Update app.js

--- Weather/app.js
+++ Weather/app.js
@@ -15,7 +15,7 @@ weather.temperature = {
 // APP CONSTS AND VARS
 const KELVIN = 273.15;
 // API KEY
-const key = "18e12341fd49404bd8fcdd39af70c4d2";
+const key = "82005d27a116c2880c8f0fcb866998a0";
 
 // CHECK IF BROWSER SUPPORTS GEOLOCATION
 if('geolocation' in navigator){
